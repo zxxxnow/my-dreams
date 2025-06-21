18ce678a2c2598f01851ced50999f86f4fbff3dc
Lil Dream <guillenquisperenzo@gmail.com>
2025-06-20 23:08:02 -0400

    Setup GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/my-dreams/', // ✅ this fixes asset paths for GitHub Pages
   plugins: [
     react(),
-    tailwindcss()    
-  ],
-})
+    tailwindcss()
+  ]
+});
